16f66dbc79b6d36e2048174566daa4d3bd590dd3
zhouzibiao <1247821135@qq.com>
2023-01-03 14:34:49 +0800

    记账页基础表单 + 底部操作栏

---
+++ packageA/page/keep-accounts/components/footer-action-bar/footer-action-bar.vue
@@ -0,0 +1,101 @@
+<template>
+  <view class="action-bar-wrapper"
+        :class="setOperationHeight">
+    <view class="action-bar"
+          :class="setOperationHeight">
+      <view class="action-bar-list">
+        <view class="action-bar-list-item"
+              v-for="items in actionList" :key="items.id">
+          <image :src="items.svg"/>
+          <view>{{ items.text }}</view>
+        </view>
+      </view>
+      <button class="action-bar-button">
+        保存
+      </button>
+    </view>
+  </view>
+</template>
+
+<script>
+import mapSvg from '../../assets/map.svg'
+import timeSvg from '../../assets/time.svg'
+import tableSvg from '../../assets/table.svg'
+
+export default {
+  props: ['operationHeight'],
+  computed: {
+    setOperationHeight() {
+      return this.operationHeight !== '96rpx' ? 'set-height' : ''
+    }
+  },
+  data() {
+    return {
+      actionList: [
+        {id: 0, svg: mapSvg, text: '位置'},
+        {id: 1, svg: timeSvg, text: '日期'},
+        {id: 2, svg: tableSvg, text: '标签'}
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.action-bar-wrapper {
+  height: 136rpx;
+}
+
+.action-bar {
+  width: 100%;
+  height: 136rpx;
+  position: fixed;
+  bottom: 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  box-shadow: 6rpx 0 16rpx 0 rgba(0, 0, 0, 0.04);
+
+  &-list {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    &-item {
+      height: max-content;
+      font-size: 22rpx;
+      color: #454c63;
+      text-align: center;
+      margin-left: 50rpx;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+
+      image {
+        width: 54rpx;
+        height: 54rpx;
+      }
+    }
+  }
+
+  &-button {
+    height: 78rpx;
+    margin: 0 26rpx;
+    padding: 0 66rpx;
+    background: #4cd964;
+    border-radius: 100rpx;
+    font-size: 30rpx;
+    color: #FFFFFF;
+    font-weight: bold;
+  }
+
+  &-button::after {
+    border: none;
+  }
+}
+
+.set-height {
+  padding-bottom: 45rpx;
+}
+</style>
--- packageA/page/keep-accounts/keep-accounts.vue
+++ packageA/page/keep-accounts/keep-accounts.vue
@@ -1,28 +1,166 @@
 <template>
-  <view>
+  <view class="accounts">
     <NavBar title="记一笔" left-icon="left"/>
-    记一笔
+    <view class="accounts-content">
+      <view class="accounts-type-tabs">
+        <view class="accounts-type-items"
+              :class="tallyType === '-' ? 'active' : ''"
+              @click="tallyType = '-'">支出</view>
+        <view class="accounts-type-items"
+              :class="tallyType === '+' ? 'active' : ''"
+              @click="tallyType = '+'">收入</view>
+      </view>
+      <view class="accounts-table">
+        <uni-icons custom-prefix="iconfont" :type="table.icon" size="88rpx"/>
+        <view>{{ table.name }}</view>
+      </view>
+      <view class="accounts-money">
+        <view>金额：</view>
+        <view class="accounts-money-input-wrapper">
+          <view>￥</view>
+          <input class="accounts-money-input"
+                 :value="money"
+                 type="digit"
+                 placeholder="请输入金额~"
+                 placeholderStyle="color:#D6D6D6"
+                 @input="onMoneyInput"/>
+        </view>
+      </view>
+      <view class="accounts-notes">
+        <view>备注</view>
+        <view class="accounts-notes-textarea-wrapper">
+          <textarea class="accounts-notes-textarea"
+                    placeholder-class	="accounts-notes-placeholder"
+                    :value="notes"
+                    type="textarea"
+                    placeholder="请输入账单备注~"
+                    :maxlength="100"
+                    @input="onNotesInput"/>
+        </view>
+      </view>
+    </view>
+    <FooterActionBar :operation-height="operationHeight"/>
   </view>
 </template>
 
 <script>
 import NavBar from "../../../components/nav-bar";
+import FooterActionBar from "./components/footer-action-bar/footer-action-bar";
+import {mapState} from 'vuex'
 
 export default {
   components: {
-    NavBar
+    NavBar,
+    FooterActionBar
+  },
+  computed: {
+    ...mapState([
+      'operationHeight'
+    ]),
   },
   data() {
     return {
-
+      tallyType: '-',
+      notes: '',
+      table: null,
+      money: null,
     }
   },
   methods: {
-
+    onNotesInput(e) {
+      this.notes = e.detail.value
+    },
+    onMoneyInput(e) {
+      this.money = e.detail.value
+    }
   }
 }
 </script>
 
 <style lang="scss">
+page {
+  background: #FFFFFF;
+}
+.accounts {
 
+  &-content {
+    margin: 32rpx 0 32rpx 26rpx;
+  }
+
+  &-type-tabs {
+    display: flex;
+    justify-content: center;
+
+    .active {
+      background: #007aff;
+      border: 1rpx solid #007aff;
+      color: #FFFFFF;
+    }
+  }
+
+  &-type-items {
+    height: 58rpx;
+    width: 112rpx;
+    border: 1rpx solid #e8e8e8;
+    font-size: 28rpx;
+    color: #454c63;
+    text-align: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 100rpx;
+    margin-right: 26rpx;
+  }
+
+  &-money {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 30rpx;
+    color: #131c38;
+    height: 76rpx;
+    margin: 80rpx 26rpx 26rpx 0;
+
+    &-input-wrapper {
+      display: flex;
+      align-items: center;
+      flex-grow: 1;
+      height: 100%;
+      border: 1px solid #e5e5e5;
+      padding: 0 26rpx;
+      margin-left: 32rpx;
+      border-radius: 13rpx;
+    }
+
+    &-input {
+      margin: 0 50rpx;
+      width: 100%;
+      text-align: right;
+    }
+  }
+
+  &-notes {
+    border-top: 1rpx solid #f2f2f2;
+    padding: 26rpx 26rpx 0 0;
+    font-size: 30rpx;
+    color: #131C38;
+
+    &-textarea-wrapper {
+      margin-top: 16rpx;
+      height: 220rpx;
+      background: #F6F6F6;
+      border-radius: 13rpx;
+      padding: 26rpx;
+    }
+
+    &-textarea {
+      width: 100%;
+      height: 100%;
+    }
+
+    &-placeholder {
+      color:#D6D6D6
+    }
+  }
+}
 </style>
