293d10da13523fcda926025f83210ea0360730d2
zhouzibiao <1247821135@qq.com>
2023-06-05 11:25:51 +0800

    style: order-info loading

--- packageA/page/order-info/order-info.vue
+++ packageA/page/order-info/order-info.vue
@@ -2,44 +2,53 @@
   <view>
     <NavBar left-icon="left"/>
     <view class="order">
-      <view class="order-header">
-        <uni-icons custom-prefix="iconfont" :type="orderInfo.table.icon" size="84rpx"/>
-        <view class="order-table">{{orderInfo.table.name}}</view>
-        <view class="order-money">{{orderInfo.type}}{{orderInfo.money}}</view>
+      <view v-if="loading"
+            class="order-loading">
+        <uni-load-more status="loading"
+                       iconSize="40"
+                       color="#bbbbbb"
+                       :showText="false"/>
       </view>
-      <view class="order-info">
-        <view class="order-info-item">
-          <view class="left-text">记录类型：</view>
-          <view class="right-text"
-                v-if="orderInfo && orderInfo.type">
-            {{onTypeText(orderInfo.type)}}
+      <view class="order-content"  v-else>
+        <view class="order-header">
+          <uni-icons custom-prefix="iconfont" :type="orderInfo.table.icon" size="84rpx"/>
+          <view class="order-table">{{ orderInfo.table.name }}</view>
+          <view class="order-money">{{ orderInfo.type }}{{ orderInfo.money }}</view>
+        </view>
+        <view class="order-info">
+          <view class="order-info-item">
+            <view class="left-text">记录类型：</view>
+            <view class="right-text"
+                  v-if="orderInfo && orderInfo.type">
+              {{ onTypeText(orderInfo.type) }}
+            </view>
+          </view>
+          <view class="order-info-item">
+            <view class="left-text">记录时间：</view>
+            <view class="right-text">
+              <uni-dateformat :date="orderInfo.date" format="yyyy年M月d日 hh:mm"/>
+            </view>
+          </view>
+          <view class="order-info-item"
+                v-if="orderInfo && orderInfo.address">
+            <view class="left-text">记录地点：</view>
+            <view class="right-text">{{ orderInfo.address.name }}</view>
+          </view>
+          <view class="order-info-item">
+            <viwe class="left-text">备注：</viwe>
+            <view class="right-text">{{ orderInfo.notes }}</view>
           </view>
         </view>
-        <view class="order-info-item">
-          <view class="left-text">记录时间：</view>
-          <view class="right-text">
-            <uni-dateformat :date="orderInfo.date" format="yyyy年M月d日 hh:mm"/>
+        <view class="order-footer">
+          <view class="order-footer-item"
+                @click="onDelete">
+            <uni-icons type="trash" size="40rpx" color="#dd524d"/>
+            <view class="footer-text delete">删除</view>
           </view>
-        </view>
-        <view class="order-info-item"
-              v-if="orderInfo && orderInfo.address">
-          <view class="left-text">记录地点：</view>
-          <view class="right-text">{{orderInfo.address.name}}</view>
-        </view>
-        <view class="order-info-item">
-          <viwe class="left-text">备注：</viwe>
-          <view class="right-text">{{orderInfo.notes}}</view>
-        </view>
-      </view>
-      <view class="order-footer">
-        <view class="order-footer-item"
-              @click="onDelete">
-          <uni-icons type="trash" size="40rpx" color="#dd524d"/>
-          <view class="footer-text delete">删除</view>
-        </view>
-        <view class="order-footer-item" @click="onEdit">
-          <uni-icons type="compose" size="40rpx" color="#9b9b9b"/>
-          <view class="footer-text">编辑</view>
+          <view class="order-footer-item" @click="onEdit">
+            <uni-icons type="compose" size="40rpx" color="#9b9b9b"/>
+            <view class="footer-text">编辑</view>
+          </view>
         </view>
       </view>
     </view>
@@ -78,11 +87,12 @@ export default {
     return {
       orderId: null,
       orderInfo: null,
-      tableInfo: null
+      loading: false,
     }
   },
   methods: {
     loadAccount(id) {
+      this.loading = true
       const wx_openid = getWxOpenId()
       uniCloud.callFunction({
         name: 'account',
@@ -94,11 +104,14 @@ export default {
         success: (res) => {
           if (res.result.status === 200) {
             this.orderInfo = res.result.account
+            this.loading = false
           } else {
+            this.loading = false
             this.showToast('账单获取失败，请稍后重试！')
           }
         },
         fail: () => {
+          this.loading = false
         }
       })
     },
@@ -145,9 +158,17 @@ export default {
 @import "../../../static/icons/iconfont.css";
 
 .order {
+  min-height: 600rpx;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   background: #FFFFFF;
   border-radius: 32rpx;
   margin: 16rpx 26rpx;
+
+  &-content {
+    width: 100%;
+  }
 
   &-header {
     padding: 72rpx 0 28rpx;
